040cfc7d8251be6f2b3113f58fc78680e8c1de5f
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-03-29 15:34:42 -0400

    about_triangle_project passing (153)

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && b == c
+    return :equilateral
+  elsif a == b || b == c || a == c
+    return :isosceles
+  elsif a != b && b != c && a != c
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
