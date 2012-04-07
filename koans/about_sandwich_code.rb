118e6aec4a361322396f254e99c837465a03f4e4
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-04-07 19:39:30 -0400

    About_sandwich_code passing	182

--- koans/about_sandwich_code.rb
+++ koans/about_sandwich_code.rb
@@ -14,7 +14,7 @@ class AboutSandwichCode < EdgeCase::Koan
   end
 
   def test_counting_lines
-    assert_equal __, count_lines("example_file.txt")
+    assert_equal 4, count_lines("example_file.txt")
   end
 
   # ------------------------------------------------------------------
@@ -29,7 +29,7 @@ class AboutSandwichCode < EdgeCase::Koan
   end
 
   def test_finding_lines
-    assert_equal __, find_line("example_file.txt")
+    assert_equal "test\n", find_line("example_file.txt")
   end
 
   # ------------------------------------------------------------------
@@ -74,33 +74,38 @@ class AboutSandwichCode < EdgeCase::Koan
   end
 
   def test_counting_lines2
-    assert_equal __, count_lines2("example_file.txt")
+    assert_equal 4, count_lines2("example_file.txt")
   end
 
   # ------------------------------------------------------------------
 
   def find_line2(file_name)
     # Rewrite find_line using the file_sandwich library function.
-  end
-
-  def test_finding_lines2
-    assert_equal __, find_line2("example_file.txt")
-  end
-
-  # ------------------------------------------------------------------
-
-  def count_lines3(file_name)
-    open(file_name) do |file|
-      count = 0
+    file_sandwich(file_name) do |file|
       while line = file.gets
-        count += 1
+        return line if line.match(/e/)
       end
-      count
     end
   end
 
-  def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal __, count_lines3("example_file.txt")
-  end
+    def test_finding_lines2
+      assert_equal "test\n", find_line2("example_file.txt")
+    end
 
-end
+    # ------------------------------------------------------------------
+
+    def count_lines3(file_name)
+      open(file_name) do |file|
+        count = 0
+        while line = file.gets
+          count += 1
+        end
+        count
+      end
+    end
+
+    def test_open_handles_the_file_sandwich_when_given_a_block
+      assert_equal 4, count_lines3("example_file.txt")
+    end
+
+  end
